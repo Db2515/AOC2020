e8fc2ca3674703827178f73bbf4a3f75d34e3553
Db2515 <daniel@boulby.plus.com>
2020-12-01 21:08:41 +0000

    Add function for three elements
    Also add functions to find unique pairs and triples to reduce
    work in list comprehension

--- ExpenseReport/ExpenseReport.hs
+++ ExpenseReport/ExpenseReport.hs
@@ -1,1 +1,11 @@
-multOnSum xs sum = [x*y | x <- xs, y <-xs, x+y == sum]
+import Data.List
+
+getUniquePairs :: [a] -> [(a,a)]
+getUniquePairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
+multOnTwoElemSum :: [Int] -> Int -> [Int] 
+multOnTwoElemSum xs sum = [x*y | (x,y) <- getUniquePairs(xs), x+y == sum]
+
+getUniqueTriples :: [a] -> [(a,a,a)]
+getUniqueTriples l = [(x,y,z) | (x:ys) <- tails l, (y:zs) <- tails ys, z <- zs]
+multOnThreeElemSum xs sum = [x*y*z | (x,y,z) <- getUniqueTriples(xs), 
+                              x+y+z == sum]
