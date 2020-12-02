06c9958ccaac75edbc08beda986a362e368c37cc
Db2515 <daniel@boulby.plus.com>
2020-12-02 20:23:04 +0000

    Add function to check valid toboggan shop passwords

--- PasswordChecker/PasswordChecker.hs
+++ PasswordChecker/PasswordChecker.hs
@@ -4,6 +4,13 @@ occurences :: Char -> [Char] -> Int
 occurences y xs = length [ x | x <-xs, x == y] 
 
 validSledShopPasswords :: [((Int, Int), Char, String)] -> [String]
-validShedShopPasswords passwords = [ pass | ((min, max), y, pass) <- passwords, 
+validSledShopPasswords passwords = [ pass | ((min, max), y, pass) <- passwords, 
                                             occurences y pass >= min,
                                             occurences y pass <= max]
+
+validTobShopPasswords :: [((Int, Int), Char, String)] -> [String]
+validTobShopPasswords passwords = [ pass | ((a, b), y, pass) <- passwords,
+                                    (((pass !! (pred a)) == y) 
+                                      && ((pass !! (pred b)) == y))
+                                    || (((pass !! (pred b)) == y)
+                                      && not ((pass !! (pred a)) == y))]
